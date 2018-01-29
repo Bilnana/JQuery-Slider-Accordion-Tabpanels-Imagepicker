1aacc618c1d550628a66ddf6f37a097d0d931f93
Bilja <biljanalj85@gmail.com>
2018-01-29 17:04:58 +0100

    first commit-Javascript exercisses arrays.

---
+++ 1.js
@@ -0,0 +1,58 @@
+/*******************************************************************************************
+1. For each print values from array *
+var numbers=[1,2,3,4,5,6];
+numbers.forEach(function(index,number){
+    console.log(index+' is '+number);
+});
+
+*/
+
+
+/*******************************************************************************************
+2. For each push items from one array to another 
+var numbers=[1,2,3,4,5,6];
+
+var otherArray=['maria','mario'];
+
+
+numbers.forEach(function(number){
+    otherArray.push(number);
+});
+
+console.log('This is numbers array:'+otherArray);
+console.log('This is otherArray array:'+numbers);
+*/
+
+
+/*******************************************************************************************
+1.w3-exercises Write a JavaScript function to check whether an `input` is an array or not *
+
+var biljana=[1,2,3,4,5]
+var h= 1;
+
+console.log(Array.isArray(biljana));
+console.log(Array.isArray(h));
+*/
+
+
+/*******************************************************************************************
+5.w3-exercises Write a simple JavaScript program to join all elements of the following array into a string 
+
+myColor = ["Red", "Green", "White", "Black"];
+
+
+var string=(myColor.join('+'));//Put elements into variable string
+
+console.log(string);*/
+
+
+
+
+
+
+
+
+
+
+
+
